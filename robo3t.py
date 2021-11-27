74f5826f1b5fcd5068900087ab3f4d72709d794e
∇IҜΔSH✯SHΔRMΔ࿐ <94558207+vikashh26@users.noreply.github.com>
2021-11-27 17:08:53 +0530

    Update robo3t.py

--- robo3t.py
+++ robo3t.py
@@ -1,2 +1,5 @@
 import os
+os.system("cd /usr/local/bin/robo3t/bin && sudo rm -rf robo3t")
+if "robo-help" in os.listdir():
+    os.system("sudo rm -rf robo-help")
 os.system("sudo mkdir robo-help && cd robo-help && sudo wget https://download.studio3t.com/robomongo/linux/robo3t-1.4.4-linux-x86_64-e6ac9ec.tar.gz?submissionGuid=f94aa69b-44e1-49e3-b57a-de75c326fa54 && sudo tar -xvzf robo3t* && sudo mv robo3t-1.4.4-linux-x86_64-e6ac9ec robo3t && sudo mv robo3t /usr/local/bin && cd /usr/local/bin/robo3t/bin && sudo chmod +x robo3t ./robo3t && ./robo3t")
